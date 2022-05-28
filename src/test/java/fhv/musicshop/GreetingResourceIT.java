957f2d908ab12de8a94bd409890073e4de9a7b76
Benjamin Mayer <lolgame99@gmail.com>
2022-05-28 12:53:54 +0200

    removed tests & changed properties

--- src/test/java/fhv/musicshop/GreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package fhv.musicshop;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
